1b6a0f6fd4bd7f3ad74b9218d0b66e0b3f9fbdca
Dietrich Travkin <dietrich.travkin@postadress.de>
2015-02-06 19:52:24 +0100

    Splitted StringCalculator class into a parser-related and a calculating class.

--- StringCalculator/src/CalculatorInput.java
+++ StringCalculator/src/CalculatorInput.java
@@ -1,3 +1,48 @@
 public class CalculatorInput {
 
+//	private static final char DEFAULT_DELIMITER = ',';
+	private static final String DELIMITER_PREFIX = "//";
+	private static final int DELIMITER_LENGTH = 1;
+	private static final String DELIMITER_SPLITTER = "\n";
+	
+	
+//	private char delimiter = ','; 
+	
+	private int[] parsedNumbers = null;
+	
+	public CalculatorInput(String textToParse) {
+		parsedNumbers = parseNumbers(textToParse);
+	}
+	
+	public int[] getCalculatorArguments() {
+		return this.parsedNumbers;
+	}
+	
+	private int[] parseNumbers(String inputText) {
+		// apply single level of abstraction:
+//		String lineWithDelimiter = extractDelimiter(inputText);
+//		String delimiter = parseDelimiter(inputText);
+//	    String textNumbers = extractNumbers(inputText);
+	    
+		
+		String stringDelimiter = "[,\n]";
+		if (inputText.startsWith(DELIMITER_PREFIX)) {
+			char delimiter = inputText.charAt(DELIMITER_PREFIX.length());
+			inputText = inputText.substring(DELIMITER_PREFIX.length()
+					+ DELIMITER_LENGTH + DELIMITER_SPLITTER.length());
+			stringDelimiter = delimiter + "";
+		}
+		int[] numbersAsInts = convertTextToNumbers(inputText, stringDelimiter);
+		return numbersAsInts;
+	}
+	
+	private int[] convertTextToNumbers(String numbers, String stringDelimiter) {
+		String[] split = numbers.split(stringDelimiter);
+		int[] numberAsInts = new int[split.length];
+		for (int i = 0; i < split.length; i++) {
+			numberAsInts[i] = Integer.parseInt(split[i]);
+		}
+		int[] numbersAsInts = numberAsInts;
+		return numbersAsInts;
+	}
 }
--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -1,30 +1,30 @@
 
 public class StringCalculator {
 
-	private static final String DELIMITER_PREFIX = "//";
-	private static final int DELIMITER_LENGTH = 1;
-	private static final String DELIMITER_SPLITTER = "\n";
-
 	public int add(String inputText) throws Exception {
-		//InputText input = new InputText(inputText);
-				
+		CalculatorInput arguments = new CalculatorInput(inputText);
+		return add(arguments);
+	}
+	
+	private int add(CalculatorInput input) {
 		int sum = 0;
 
-		if (inputText.isEmpty()) {
+		int[] arguments = input.getCalculatorArguments();
+		if (arguments == null || arguments.length == 0)
+		{
 			return sum;
 		}
 
-		int[] numbers = parseNumbers(inputText);
-		checkForNegativeNumbers(numbers);
+		checkForNegativeNumbers(arguments);
 
-		for (int element : numbers) {
+		for (int element : arguments) {
 
 			sum += element;
 		}
 		return sum;
 	}
 
-	private void checkForNegativeNumbers(int[] numbers) throws Exception {
+	private void checkForNegativeNumbers(int[] numbers) {
 		String negativeNumbers = null;
 		for (int element : numbers) {
 			if (element < 0) {
@@ -36,43 +36,11 @@ public class StringCalculator {
 			}
 		}
 		if (negativeNumbers != null) {
-			throw new Exception(
+			throw new IllegalArgumentException(
 					"Negative Zahlen sind nicht erlaubt. Folgende wurden gefunden: "
 							+ negativeNumbers);
 		}
 		return;
 	}
-
-//	private int add(InputText input) {
-//		int[] numbers= input.getLisdtOfNumbers();
-//		
-//	}
 	
-	private int[] parseNumbers(String inputText) {
-//		String lineWithDelimiter = extractDelimiter(inputText);
-//		String delimiter = parseDelimiter(inputText);
-//	    String textNumbers = extractNumbers(inputText);
-	    
-		
-		String stringDelimiter = "[,\n]";
-		if (inputText.startsWith(DELIMITER_PREFIX)) {
-			char delimiter = inputText.charAt(DELIMITER_PREFIX.length());
-			inputText = inputText.substring(DELIMITER_PREFIX.length()
-					+ DELIMITER_LENGTH + DELIMITER_SPLITTER.length());
-			stringDelimiter = delimiter + "";
-		}
-		int[] numbersAsInts = convertTextToNumbers(inputText, stringDelimiter);
-		return numbersAsInts;
-	}
-	
-	private int[] convertTextToNumbers(String numbers, String stringDelimiter) {
-		String[] split = numbers.split(stringDelimiter);
-		int[] numberAsInts = new int[split.length];
-		for (int i = 0; i < split.length; i++) {
-			numberAsInts[i] = Integer.parseInt(split[i]);
-		}
-		int[] numbersAsInts = numberAsInts;
-		return numbersAsInts;
-	}
-
 }
