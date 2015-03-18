7baf8bba7d155c986588668b5994dcf4c0043731
Bastian Seehaus <itbastian@quantentunnel.de>
2015-03-18 17:02:36 +0100

    Refactoring Karsten, Bastian

--- StringCalculator/src/CalculatorInput.java
+++ StringCalculator/src/CalculatorInput.java
@@ -1,45 +1,60 @@
 public class CalculatorInput {
 
+	private static final String DEFAULT_DELIMITER = "[,\n]";
 	private static final String DELIMITER_PREFIX = "//";
 	private static final int DELIMITER_LENGTH = 1;
 	private static final String DELIMITER_SPLITTER = "\n";
 
-	
-	private int[] parsedNumbers = null;
-	
+	private int[] parsedNumbers;
+	private String inputText;
+	private String currentDelimiter;
+
 	public CalculatorInput(String textToParse) {
-		parsedNumbers = parseNumbers(textToParse);
+		this.inputText = textToParse;
+		parseDelimiter();
+		parseNumbers();
 	}
-	
+
+	private void parseDelimiter() {
+		if (startsWithDelimiterPrefix()) {
+			char delimiter = extractDelimiter(inputText);
+			currentDelimiter = String.valueOf(delimiter);
+		} else {
+			currentDelimiter = DEFAULT_DELIMITER;
+		}
+	}
+
 	public int[] getCalculatorArguments() {
 		return this.parsedNumbers;
 	}
-	
-	private int[] parseNumbers(String inputText) {
-		if (inputText.isEmpty()){
-			return new int[]{0};
+
+	private void parseNumbers() {
+		String numbersText = extractNumbersPart();
+		if (numbersText.isEmpty()) {
+			parsedNumbers = new int[] { 0 };
+		} else {
+			parsedNumbers = convertTextToNumbers(numbersText);
 		}
-		String stringDelimiter = "[,\n]";
-		if (inputText.startsWith(DELIMITER_PREFIX)) {
-			char delimiter = extractDelimiter(inputText);
-			inputText = extractNumbersPart(inputText);
-			stringDelimiter = delimiter + "";
-		}
-		int[] numbersAsInts = convertTextToNumbers(inputText, stringDelimiter);
-		return numbersAsInts;
 	}
 
-	private String extractNumbersPart(String inputText) {
-		return inputText.substring(DELIMITER_PREFIX.length()
-				+ DELIMITER_LENGTH + DELIMITER_SPLITTER.length());
+	private String extractNumbersPart() {
+		if (startsWithDelimiterPrefix()) {
+			return inputText.substring(DELIMITER_PREFIX.length() + DELIMITER_LENGTH
+					+ DELIMITER_SPLITTER.length());
+		}
+		return inputText;
+	}
+
+	private boolean startsWithDelimiterPrefix() {
+		return inputText.startsWith(DELIMITER_PREFIX);
 	}
 
 	private char extractDelimiter(String inputText) {
 		return inputText.charAt(DELIMITER_PREFIX.length());
 	}
-	
-	private int[] convertTextToNumbers(String numbers, String stringDelimiter) {
-		String[] split = numbers.split(stringDelimiter);
+
+	private int[] convertTextToNumbers(String numbersText) {
+		String[] split = numbersText.split(currentDelimiter);
 		int[] numbersAsInts = new int[split.length];
 		for (int i = 0; i < split.length; i++) {
 			numbersAsInts[i] = Integer.parseInt(split[i]);
