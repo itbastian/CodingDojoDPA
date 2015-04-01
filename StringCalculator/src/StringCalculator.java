f4a02b25e8795387d23251fa3603d15f22414e77
RuepInc <thomas.rueping@googlemail.com>
2015-04-01 11:05:32 +0200

    Refactoring von Andree und Thomas

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -1,11 +1,6 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-
 public class StringCalculator {
 
-	public int add(String inputText) throws Exception {
+	public int add(String inputText) {
 		CalculatorInput arguments = new CalculatorInput(inputText);
 		return add(arguments);
 	}
@@ -13,44 +8,15 @@ public class StringCalculator {
 	private int add(CalculatorInput input) {
 
 		int[] arguments = input.getCalculatorArguments();
-
-		abortIfNegativeNumbersAreContained(arguments);
+		
+		final InputValidator validator = new InputValidator();
+		validator.abortIfNegativeNumbersAreContained(arguments);
 
 		int sum = 0;
 		for (int argument : arguments) {
 			sum += argument;
 		}
 		return sum;
-	}
-
-	private void abortIfNegativeNumbersAreContained(int[] numbers) {
-		List<Integer> negativeNumbers = new ArrayList<Integer>();
-		for (int element : numbers) {
-			if (element < 0) {
-				negativeNumbers.add(element);
-			}
-		}
-		
-		if (!negativeNumbers.isEmpty()) {
-			throw new IllegalArgumentException(
-					"Negative Zahlen sind nicht erlaubt. Folgende wurden gefunden: "
-							+ join(negativeNumbers));
-		}
-		return;
-	}
-
-	private String join(List<Integer> negativeNumbers) {
-		Iterator<Integer> iterator = negativeNumbers.iterator();
-		StringBuilder str = new StringBuilder();
-		
-		while(iterator.hasNext()) {
-			str.append(iterator.next());
-			if(iterator.hasNext()) {
-				str.append(",");
-			}
-		}
-		
-		return str.toString();
 	}
 	
 }
