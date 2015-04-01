b51f3f6699d17ade74a816c4b286af9a6985a917
trav010 <travkin@gmx.de>
2015-04-01 16:38:59 +0200

    Refactoring to add other operations

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -1,20 +1,35 @@
 public class StringCalculator {
+	
+	private enum Operation {
+		ADD;
+	}
 
 	public int add(String inputText) {
 		CalculatorInput arguments = new CalculatorInput(inputText);
-		return add(arguments);
+		return calculate(arguments);
 	}
 	
-	private int add(CalculatorInput input) {
+	private int add(int value1, int value2) {
+		return value1 + value2;
+	}
 
+	private int calculate(CalculatorInput input) {
 		int[] arguments = input.getCalculatorArguments();
+		Operation operation = Operation.ADD;
 		
 		final InputValidator validator = new InputValidator();
 		validator.abortIfNegativeNumbersAreContained(arguments);
 
 		int sum = 0;
 		for (int argument : arguments) {
-			sum += argument;
+			switch (operation) {
+			case ADD:
+				sum = add(sum, argument);
+				break;
+
+			default:
+				break;
+			}
 		}
 		return sum;
 	}
